2f6fe15705a657c5fca7fbab3d77ac1b2b3a0db5
sexychoco <gaeun819@naver.com>
2022-02-08 23:54:42 +0900

    frontend setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+
 console.log("Hello!");
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
